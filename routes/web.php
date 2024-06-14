25bbff98545513799e982e2063cac6ec080267fe
ramdhannassyirah <121411843+ramdhannassyirah@users.noreply.github.com>
2024-06-14 15:28:10 +0700

    Feat/landing page up (#16)
    * feat: Landing Page Up
    
    * fix: Landing Page Up
    
    ---------
    
    Co-authored-by: ramdhannassyirahh <ramdhanassyirahh@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/home', function () {
+    return view('components.pages.frontend.index');
+});
+
 require __DIR__.'/auth.php';
