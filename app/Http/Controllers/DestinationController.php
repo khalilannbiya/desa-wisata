36937754008c8c9ba2685a69cf7e11778c347c5f
slamets24 <slametseptiawan9d@gmail.com>
2024-06-17 14:09:40 +0700

    feat/read detail destination functionality

---
+++ app/Http/Controllers/DestinationController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Destination;
+use App\Models\user;
+use Illuminate\Support\Facades\Auth;
+
+class DestinationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $detail = Destination::with(['user', 'galleries', 'openingHours', 'accommodations', 'facilities', 'contactDetails'])
+            ->where('id', $id)
+            ->firstOrFail();
+
+        return view('', compact('$detail'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DestinationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/detail/{id}', [DestinationController::class, 'show']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
