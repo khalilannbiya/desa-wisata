f2b60024b81baeb86569c9e6a48c791c8a6653e9
ramdhannassyirahh <ramdhanassyirahh@gmail.com>
2024-06-18 16:54:19 +0700

    fix: Nambah favicon

--- resources/views/components/pages/frontend/index.blade.php
+++ resources/views/components/pages/frontend/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}" type="image/x-icon">
     <title>DESA WISATA</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
