551e5022c2d7304e9d325b9aeb41604ac309ff50
ramdhannassyirahh <ramdhanassyirahh@gmail.com>
2024-06-19 13:19:56 +0700

    fix: Landing-Page

--- resources/views/components/partials/frontend/card--event.blade.php
+++ resources/views/components/partials/frontend/card--event.blade.php
@@ -1,38 +1,44 @@
-
-    <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-        <div class="h-96 w-73">
-          <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="{{asset('assets/img/dummy-vilage.jpg')}}" alt="" />
-        </div>
-        <div class="sm:max-w-sm absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-        <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-          <h1 class="font-dmserif text-3xl font-bold text-white">Beauty</h1>
-          <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
-          <button class="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60">See More</button>
-        </div>
-      </div>      
+<div
+    class="group relative cursor-pointer rounded-md items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+    <div class="h-96 w-73">
+        <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src="{{ asset('assets/img/dummy-vilage.jpg') }}" alt="" />
+    </div>
+    <div
+        class="sm:max-w-sm absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+    </div>
+    <div
+        class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <h1 class="font-dmserif text-3xl font-bold text-white">Beauty</h1>
+        <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+        <button
+            class="rounded-full bg-neutral-900 px-3.5 py-2 font-com text-sm capitalize text-white shadow shadow-black/60">See
+            More</button>
+    </div>
+</div>
 
 
 <script>
-  // Text Elipsis via javascript
-  function shortenText(elementSelector, maxLength, elipsis) {
-            let elements = document.querySelectorAll(elementSelector);
+    // Text Elipsis via javascript
+    function shortenText(elementSelector, maxLength, elipsis) {
+        let elements = document.querySelectorAll(elementSelector);
 
-            elements.forEach(function(element) {
-                let textContent = element.textContent.trim();
+        elements.forEach(function(element) {
+            let textContent = element.textContent.trim();
 
-                if (textContent.length > maxLength) {
-                    if (elipsis) {
-                        let shortenedContent =
-                            textContent.substring(0, maxLength) + " ...";
-                        element.textContent = shortenedContent;
-                    } else {
-                        let shortenedContent = textContent.substring(0, maxLength);
-                        element.textContent = shortenedContent;
-                    }
+            if (textContent.length > maxLength) {
+                if (elipsis) {
+                    let shortenedContent =
+                        textContent.substring(0, maxLength) + " ...";
+                    element.textContent = shortenedContent;
+                } else {
+                    let shortenedContent = textContent.substring(0, maxLength);
+                    element.textContent = shortenedContent;
                 }
-            });
-        }
+            }
+        });
+    }
 
-        shortenText(".elipsis", 250, true);
-
+    shortenText(".elipsis", 250, true);
 </script>
--- resources/views/components/partials/frontend/card-article.blade.php
+++ resources/views/components/partials/frontend/card-article.blade.php
@@ -1,35 +1,43 @@
-        {{--card  --}}
-        <div class="max-w-sm shadow-lg">
+        {{-- card  --}}
+        <div class="max-w-sm shadow-lg border-2 rounded-md ">
             <div class="p-6">
                 <div class="pb-3 mb-4 border-b border-stone-200 text-xs font-medium flex justify-between text-blue-900">
-                <span class="flex items-center gap-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    7 March, 2022
-                </span>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-                    </svg>
-                </span>
+                    <span class="flex items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        7 March, 2022
+                    </span>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+                        </svg>
+                    </span>
                 </div>
-                <h3 class="mb-4 font-semibold  text-2xl"><a href="" class="transition-all text-blue-900 hover:text-blue-600">laboriosam doloribus quas. Voluptatum</a></h3>
+                <h3 class="mb-4 font-semibold  text-2xl"><a href=""
+                        class="transition-all text-blue-900 hover:text-blue-600">laboriosam doloribus quas.
+                        Voluptatum</a></h3>
                 <p class="text-sky-800 text-sm mb-0">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore laborum iste laboriosam doloribus quas. Voluptatum
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore laborum iste laboriosam doloribus
+                    quas. Voluptatum
                 </p>
             </div>
             <div class="mt-auto">
                 <img src="https://picsum.photos/400/300" alt="" class="w-full h-48 object-cover">
             </div>
             <div class="text-center">
-                <a href="#" class="my-6 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-deep-koamaru-600 rounded hover:bg-deep-koamaru-500 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                <a href="#"
+                    class="my-6 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-deep-koamaru-600 rounded hover:bg-deep-koamaru-500 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                     Detail Artikel
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
                 </a>
             </div>
-        </div> 
-       
-        
+        </div>
