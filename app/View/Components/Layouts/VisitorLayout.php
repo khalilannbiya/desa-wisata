2068154a2478b6438f7101991db7e8077bfbf032
ramdhannassyirah <121411843+ramdhannassyirah@users.noreply.github.com>
2024-06-22 19:01:47 +0700

    feat: detail-page (#50)
    Co-authored-by: ramdhannassyirahh <ramdhanassyirahh@gmail.com>

--- app/View/Components/Layouts/VisitorLayout.php
+++ app/View/Components/Layouts/VisitorLayout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Navbar extends Component
+class VisitorLayout extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class Navbar extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.layouts.navbar');
+        return view('components.layouts.visitor-layout');
     }
 }
