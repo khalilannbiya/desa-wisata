df5b767cd2901ca277dbe0b137769a96b37e3160
annisa-ozka <annisaputri.sb@gmail.com>
2024-06-24 11:47:12 +0700

    feat:about-us-page

---
+++ resources/views/components/pages/frontend/about-us-page.blade.php
@@ -0,0 +1,18 @@
+<x-Layouts.navbar>
+    <x-slot:title>Beranda | </x-slot:title>
+    <header>
+        <x-partials.frontend.header />
+    </header>
+
+    <section class="justify-center">
+        <x-partials.frontend.description/>
+    </section>
+
+    <section class="mt-20">
+        <x-partials.frontend.footer />
+    </section>
+
+
+    
+
+</x-Layouts.navbar>
