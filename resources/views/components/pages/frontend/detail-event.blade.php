b2a777d746ce78425aa7884401fc0072afce4d56
Annisa Putri <127081797+annisa-ozka@users.noreply.github.com>
2024-06-25 16:19:34 +0700

    feat:detailEvent (#66)
    Co-authored-by: Syeich Khalil Annbiya <84386261+khalilannbiya@users.noreply.github.com>

---
+++ resources/views/components/pages/frontend/detail-event.blade.php
@@ -0,0 +1,32 @@
+<x-layouts.visitor-layout>
+<div class="lg:flex lg:flex-col-2 lg:pt-10 ">  
+    <div class=" pt-29 mr-0 max-w-screen-xl px-4 pb-0 py-8 mx-auto relative z-10 flex justify-center">
+        <img class="lg:w-[600px] lg:h-[400px] p-3 sm:w-[500px] sm:h-[300px] md:w-[600px] md:h-[400px] w-full lg:mb-0 lg:flex gs_reveal gs_reveal_fromRight" src="http://desa-wisata.test:8080/assets/img/dummy-vilage.jpg" alt="dashboard feature image" style="opacity: 1; visibility: inherit; transform: translate(0px, 0px); translate: none; rotate: none; scale: none;">
+    </div>
+    <main class="pt-5 sm:mt-10 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+        <div class="mr-12 border-2 border-black bg-green-300 flex justify-between px-4 mx-7 max-w-screen-xl ">
+            <article class=" mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                <header class="lg:mb-6 not-format">
+                    <h1 class="p-5 text-center text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">Best practices for successful prototypes</h1>
+                </header>
+                <div class="m-5 p-2 pt-0">
+                    <p class="lead">!!!Flowbite is an open-source library of UI components built with the utility-first
+                        classes from Tailwind CSS. It also includes interactive elements such as dropdowns, modals,
+                        datepickers.</p>
+                    <p>Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This way,
+                        you can think things through before committing to an actual design project.</p>
+                    <p>But then I found a <a href="https://flowbite.com">component library based on Tailwind CSS called
+                            Flowbite</a>. It comes with the most commonly used UI components, such as buttons, navigation
+                        bars, cards, form elements, and more which are conveniently built with the utility classes from
+                        Tailwind CSS.</p>
+                    <p>Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This way,
+                            you can think things through before committing to an actual design project.</p>
+                        
+                </div>
+                <div class="m-5">      
+                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                        <a href="/home">Kembali </a></button>
+                </div>
+                
+</div>
+</x-layouts.visitor-layout>
