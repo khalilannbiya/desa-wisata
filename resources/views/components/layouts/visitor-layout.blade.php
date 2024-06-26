849475e2196ce90f74408876a4201333fe4ec4b1
ramdhannassyirah <121411843+ramdhannassyirah@users.noreply.github.com>
2024-06-26 10:24:46 +0700

    fix: footer (#70)
    Co-authored-by: ramdhannassyirahh <ramdhanassyirahh@gmail.com>

--- resources/views/components/layouts/visitor-layout.blade.php
+++ resources/views/components/layouts/visitor-layout.blade.php
@@ -86,10 +86,9 @@
         {{ $slot }}
     </section>
 
-    <footer class="w-full mt-20 text-gray-700 bg-gray-100 body-font">
-        <div
-            class="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
-            <div class="flex-shrink-0 w-64 mx-auto text-center md:mx-0 ">
+    <footer class="w-full  mt-20 text-gray-700 bg-gray-100 body-font">
+        <div class="flex flex-col md:flex-row items-center  px-5 py-24  ">
+            <div class="lg:w-1/2  mx-auto text-center md:mx-0 ">
                 <a class="flex items-center justify-center font-medium text-gray-900 title-font ">
                     <img class="w-20" src="{{ asset('assets/img/logo.png') }}" alt="">
                 </a>
@@ -118,80 +117,43 @@
                                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                             </svg>
                         </a>
-                        <a class="ml-3 text-gray-500 cursor-pointer hover:text-green-new">
-                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
-                                stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
-                                <path stroke="none"
-                                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
-                                </path>
-                                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                            </svg>
-                        </a>
+
                     </span>
                 </div>
             </div>
             <div class="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">About
-                    </h2>
-                    <nav class="mb-10 list-none">
+
+                <div class="w-full px-4 md:w-1/2">
+
+                    <nav class="mb-10 list-none text-xl">
                         <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Company</a>
+                            <a href="/" class="text-gray-500 cursor-pointer hover:text-green-new">Beranda</a>
                         </li>
                         <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Careers</a>
+                            <a href="/destinations"
+                                class="text-gray-500 cursor-pointer hover:text-green-new">Wisata</a>
                         </li>
                         <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Blog</a>
-                        </li>
-                    </nav>
-                </div>
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Support
-                    </h2>
-                    <nav class="mb-10 list-none">
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Contact Support</a>
-                        </li>
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Help Resources</a>
-                        </li>
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Release Updates</a>
+                            <a href="/galeri" class="text-gray-500 cursor-pointer hover:text-green-new">Galeri</a>
                         </li>
                     </nav>
                 </div>
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">
-                        Platform
-                    </h2>
-                    <nav class="mb-10 list-none">
+                <div class="w-full px-4  md:w-1/2">
+
+                    <nav class="mb-10 list-none text-xl">
                         <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Terms &amp; Privacy</a>
+                            <a href="/event" class=" cursor-pointer hover:text-green-new">Acara</a>
                         </li>
                         <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Pricing</a>
+                            <a href="/artikel" class=" cursor-pointer hover:text-green-new">Artikel</a>
                         </li>
                         <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">FAQ</a>
+                            <a href="/aboutus" class=" cursor-pointer hover:text-green-new">Tentang Kami</a>
                         </li>
                     </nav>
                 </div>
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Contact
-                    </h2>
-                    <nav class="mb-10 list-none">
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Send a Message</a>
-                        </li>
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">Request a Quote</a>
-                        </li>
-                        <li class="mt-3">
-                            <a class="text-gray-500 cursor-pointer hover:text-green-new">+123-456-7890</a>
-                        </li>
-                    </nav>
-                </div>
+
+
             </div>
         </div>
         <div class="bg-green-new">
