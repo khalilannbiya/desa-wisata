8679a2ce7695572658bd8539bb414cdd23a96d0b
Annisa Putri <127081797+annisa-ozka@users.noreply.github.com>
2024-07-01 11:32:37 +0700

    fix:aboutuspage (#106)

--- resources/views/components/partials/frontend/description.blade.php
+++ resources/views/components/partials/frontend/description.blade.php
@@ -2,10 +2,10 @@
 <div class="grid grid-cols-1 font-inter">
 
     <div class="items-center">
-        <p class="font-bold text-center m-5 lg:mb-8 text-[22px] mb-0 lg:text-[30px]">
+        <p class="font-bold text-center m-5 lg:mb-8 text-[22px] mb-0 lg:text-[25px]">
             Visi Desa Sukarame
         </p>
-        <p class="lg:px-5 text-[17px] m-10 mb-0 mt-5 text-center font-normal text-lg lg:mb-8 lg:text-[25px] ">
+        <p class="lg:px-5 text-[17px] m-10 mb-0 mt-5 text-center font-normal text-lg lg:mb-8 lg:text-[20px] ">
             Mewujudkan masyarakat yang tentram, makmur, dan berdaya saing Berlandaskan religius, kultural, dan
             berwawasan lingkungan
         </p>
@@ -40,23 +40,20 @@
         </div>
     </div>
 
-
-
-    <p class="text-center font-bold text-lg text-[22px] lg:mb-8 lg:text-[30px]">
+    <p class="text-center font-bold text-lg text-[22px] lg:mb-8 lg:text-[25px]">
         Misi Desa Sukarame
     </p>
     <div class="flex justify-center">
-        <p
-            class="text-[17px] mt-0 ml-8 p-7 text-pretty m-5 max-w-2xl lg:max-w-5xl mb-0 font-normal text-lg lg:mb-10 lg:text-[25px] font-inter">
-            1. Peningkatan kualitas SDM <br><br>
-            2. Menciptakan pembangunan ekonomi yang berdaya saing <br><br>
-            3. Menciptakan pemerintahan desa yang Transparan<br><br>
-            4. Meningkatkan pelayanan yang prima terhadap warga masyarakat<br><br>
-            5. Meningkatkan kepedulian terhadap warga masyarakat yang kurang mampu<br><br>
-            6. Meningkatkan pelayanan air bersih kepada warga masyarakat<br><br>
-            7. Menumbuhkembangkan olah raga dan kesenian<br><br>
-            8. Mendorong peran aktif BUMDES untuk meningkatkan ekonomi masyarakat
-        </p>
+        <ol class="list-decimal text-[17px] mt-0 ml-8 p-7 text-pretty m-5 max-w-2xl lg:max-w-5xl mb-0 font-normal text-lg lg:mb-4 lg:text-[20px] font-inter">
+            <li>Peningkatan kualitas SDM</li><br>
+            <li>Menciptakan pembangunan ekonomi yang berdaya saing</li><br>
+            <li>Menciptakan pemerintahan desa yang Transparan</li><br>
+            <li>Meningkatkan pelayanan yang prima terhadap warga masyarakat</li><br>
+            <li>Meningkatkan kepedulian terhadap warga masyarakat yang kurang mampu</li><br>
+            <li>Meningkatkan pelayanan air bersih kepada warga masyarakat</li><br>
+            <li>Menumbuhkembangkan olah raga dan kesenian</li><br>
+            <li>Mendorong peran aktif BUMDES untuk meningkatkan ekonomi masyarakat</li><br>
+        </ol>
     </div>
 </div>
 
@@ -64,12 +61,12 @@
 <div class="container px-4 pt-0 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-10 lg:pt-0">
     <div class="flex flex-col">
         <div class="m-5 text-lg font-semibold text-center lg:mb-5 lg:text-xl">
-            <p class="text-center font-bold text-lg lg:mb-4 text-[22px] lg:text-[30px] font-inter">
+            <p class="text-center font-bold text-lg lg:mb-4 text-[22px] lg:text-[25px] font-inter">
                 Sejarah Desa Sukarame
             </p>
         </div>
         <div class="flex p-3">
-            <p class="p-2 pt-0 text-center mb-3 text-black-500 dark:text-gray-400 text-[17px] lg:text-[25px] lg:mb-10 font-inter">
+            <p class="p-2 pt-0 text-center mb-3 text-black-500 dark:text-gray-400 text-[17px] lg:text-[20px] lg:mb-6 font-inter">
                 Desa Sukarame berasal dari dua suku kata yaitu "suka" dan "rame". Konon menurut cerita rakyat bahwa
                 pemimpin pertama atau Kepala Desa Sukarame pertama adalah yang memberi nama tersebut yaitu Nyi Mas
                 Sairiningrum (1918-1925), bahwa dia suka akan keramaian disaat musyawarah dengan saudara-saudara dan
@@ -81,7 +78,7 @@
 
 <!-- Section Lokasi dengan Peta -->
 <div class="mb-4 text-center">
-    <h1 class="text-[23px] font-bold font-inter lg:text-[30px]">Lokasi Kami</h1>
+    <h1 class="text-[23px] font-bold font-inter lg:text-[25px]">Lokasi Kami</h1>
 </div>
 <div class="p-5">
     <iframe class="mx-auto max-w-7xl"
